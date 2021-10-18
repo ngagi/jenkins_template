087bb095d4cf14bda049a2eb90a9712c829af2ab
nlukmanova <look.nata@gmail.com>
2021-10-18 20:40:32 +0300

    add simple tests

---
+++ src/test/java/tests/simple/SkippedTests.java
@@ -0,0 +1,27 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SkippedTests {
+    @Test
+    @Disabled
+    void test0() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled
+    void test1() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled("With some reason")
+    void test2() {
+        assertTrue(false);
+    }
+
+}
